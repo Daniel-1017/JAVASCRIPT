36d7b3bbe5dc1958199e2876d37427bd444f0599
Daniel-17 <frimudaniel17@gmail.com>
2023-02-14 17:03:51 +0100

    let, const & var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,3 +4,13 @@ const string = "string"
 const boolean = true
 const _null = null
 let _undefined
+
+// let, const & var
+let age = 10 // variable that can be mutated
+age = 11
+
+const birthYear = 2000 // variable that can't be mutated
+// birthYear = 2001 WRONG
+
+var job = "programmer"
+job = "teacher"
