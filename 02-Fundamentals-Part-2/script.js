5a7d6b06a01c03236434639f95891ad6b8a5e9a6
Daniel-17 <frimudaniel17@gmail.com>
2023-02-15 12:46:47 +0100

    functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -3,7 +3,27 @@
 let hasDriversLicense = false
 const passTest = true
 
-if (passTest) hasLicense = true
-if (hasDriversLicense) console.log("I can drive.")
+// if (passTest) hasLicense = true
+// if (hasDriversLicense) console.log("I can drive.")
 
-const interface = "Audio"
+// const interface = "Audio"
+
+function logger() {
+  console.log("My name is Jonas.")
+}
+
+// calling / running / invoking function
+// logger()
+
+function fruitProcessor(apples, oranges) {
+  // apples and oranges are parameters
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+  return juice
+}
+
+// 5 and 0 are arguments
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(3, 2)
+console.log(appleOrangeJuice)
