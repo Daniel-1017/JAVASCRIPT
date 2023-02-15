39bee96885eb907ab54ed9abe2d390e931d42e98
Daniel-17 <frimudaniel17@gmail.com>
2023-02-15 19:41:10 +0100

    handling click events

--- 03-DOM-and-Events-Fundamentals/Guess-My-Number/script.js
+++ 03-DOM-and-Events-Fundamentals/Guess-My-Number/script.js
@@ -1,6 +1,16 @@
 "use strict"
 
+/* 
 document.querySelector(".message").textContent = "Correct Number"
 document.querySelector(".number").textContent = 13
 document.querySelector(".score").textContent = 5
 document.querySelector(".guess").value = 10
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value)
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No number!"
+  }
+})
