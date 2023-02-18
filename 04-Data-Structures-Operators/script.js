f65ce25db9e437afa7ec0b2517c87cb335e16256
Daniel-17 <frimudaniel17@gmail.com>
2023-02-18 12:37:44 +0100

    coding challenge

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -479,3 +479,27 @@ for (const [key, val] of question) {
 
 // if (answer === question.get("correct")) console.log(question.get(true))
 // else console.log(question.get(false))
+
+// Coding challenge #3
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+])
+
+const events = [...new Set(gameEvents.values())]
+gameEvents.delete(64)
+const lastMin = [...gameEvents.keys()].pop()
+console.log(`An event happened, on average, every ${lastMin / gameEvents.size}`)
+
+for (let [min, event] of gameEvents) {
+  console.log(`[${min <= 45 ? "FIRST" : "SECOND"} HALF] ${min}: ${event}`)
+}
