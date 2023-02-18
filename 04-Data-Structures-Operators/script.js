89e4c214325176a6779823731e9529209e10fa82
Daniel-17 <frimudaniel17@gmail.com>
2023-02-18 10:50:20 +0100

    coding challenge

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -378,3 +378,72 @@ console.log("🚀 ~ file: script.js:376 ~ entries", entries)
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open ad ${open} and close at ${close}`)
 }
+
+// Coding challenge #2
+
+/* 
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+}
+*/
+
+// 1.
+for (const [goal, player] of game.scored.entries()) {
+  console.log(`Goal ${+goal + 1}, ${player}`)
+}
+
+// 2.
+const odds = Object.values(game.odds)
+let average = 0
+for (const odd of odds) average += odd
+average /= Object.values(odds).length
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`
+  console.log(`Odd of ${teamStr} ${odd}`)
+}
+
+// BONUS
+const scores = {}
+for (const player of Object.values(game.scored)) {
+  scores[player] ? scores[player]++ : (scores[player] = 1)
+}
+console.log(scores)
