9006fa417cefb7c902a99351ce9e54455d6452da
Daniel-17 <frimudaniel17@gmail.com>
2023-02-19 17:08:20 +0100

    passing values

--- 05-Functions/script.js
+++ 05-Functions/script.js
@@ -1,5 +1,6 @@
 "use strict"
 
+// DEFAULT PARAMETERS
 const bookings = []
 
 const createBooking = function (
@@ -29,3 +30,33 @@ createBooking("LH123", 4)
 
 // skip a default parameter
 createBooking("LH123", undefined, 4)
+
+// HOW PASSING ARGUMENTS WORKS
+const flight = "LH234"
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 9238465707,
+}
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999"
+  passenger.name = "Mr. " + passenger.name
+
+  if (passenger.passport === 9238465707) console.log("Check in.")
+  else console.log("Wrong passport.")
+}
+
+checkIn(flight, jonas)
+
+/* 
+Passing a primitive type to a function is like creating a copy of it
+
+Passing a object to a function is like passing a reference
+*/
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000000)
+}
+
+newPassport(jonas)
+checkIn(flight, jonas)
