386463492797a0f3951fe3c360947bb86de8dbe7
Daniel-17 <frimudaniel17@gmail.com>
2023-02-19 16:55:36 +0100

    default parameters

--- 05-Functions/script.js
+++ 05-Functions/script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict"
+
+const bookings = []
+
+const createBooking = function (
+  flight,
+  passengers = 1,
+  price = 199 * passengers
+) {
+  /*
+  ES5 
+    passengers = passengers || 1
+    price = price || 199
+     */
+
+  const booking = {
+    flight,
+    passengers,
+    price,
+  }
+
+  console.log(booking)
+  bookings.push(booking)
+}
+
+createBooking("LH123")
+createBooking("LH123", 2, 800)
+createBooking("LH123", 4)
+
+// skip a default parameter
+createBooking("LH123", undefined, 4)
