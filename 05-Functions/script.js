93f90c84b950d6214ef0c7523dba9a0716c90a68
Daniel-17 <frimudaniel17@gmail.com>
2023-02-20 15:59:12 +0100

    more on closures

--- 05-Functions/script.js
+++ 05-Functions/script.js
@@ -254,3 +254,40 @@ booker()
 // A closure gives a function access to all the variables of its parent function, even after that parent function has returnde. The function keeps a reference to its outre scope, which preserves the scope chain throughout time.
 
 // Closure examples
+// EX 1
+let f
+
+const g = function () {
+  const a = 23
+  f = function () {
+    console.log(a * 2)
+  }
+}
+
+const h = function () {
+  const b = 777
+  f = function () {
+    console.log(b * 2)
+  }
+}
+
+g()
+f()
+
+h()
+f()
+
+// EX 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers.`)
+    console.log(`There are 3 groups, each with ${perGroup} passengers.`)
+  }, wait * 1000)
+
+  console.log(`Will start boarding in ${wait} seconds.`)
+}
+
+const perGroup = 1000
+boardPassengers(180, 3)
