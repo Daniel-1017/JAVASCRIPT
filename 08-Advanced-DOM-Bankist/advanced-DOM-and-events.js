613aa12ae2212a2a61d239d48528b2ecd80dbdde
Daniel-17 <frimudaniel17@gmail.com>
2023-02-23 19:25:00 +0100

    styles, attributes and classes

--- 08-Advanced-DOM-Bankist/advanced-DOM-and-events.js
+++ 08-Advanced-DOM-Bankist/advanced-DOM-and-events.js
@@ -20,4 +20,39 @@ message1.classList.add("cookie-message")
 message1.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn-close-cookie">Got it!</button>'
 
-console.log("🚀 ~ file: advanced-DOM-and-events.js:18 ~ message:", message1)
+// Inline Styles
+message.style.backgroundColor = "#37383d"
+
+// Getting styles
+console.log(getComputedStyle(message).backgroundColor)
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px"
+
+// document.documentElement.style.setProperty("--color-primary", "orangered")
+
+// Attributes
+const logo = document.querySelector(".nav__logo")
+console.log(logo.alt)
+console.log(logo.src) // absolute path
+console.log(logo.getAttribute("src")) // relative path
+console.log(logo.className)
+
+logo.alt = "Beautiful minimalist logo"
+
+// Non-standard
+console.log(logo.designer) // undefined
+console.log(logo.getAttribute("designer"))
+logo.setAttribute("company", "bankist")
+
+// Data attributes
+console.log(logo.dataset.versionNumber)
+
+// Classes
+logo.classList.add("fake-class-name")
+logo.classList.remove("fake-class-name")
+logo.classList.toggle("fake-class-name")
+logo.classList.contains("fake-class-name")
+
+// This will overwrite old classes
+// logo.className = "Jonas"
