eaed134b2bfc1a33a5c0317a9b07b92dab42b98d
Daniel-17 <frimudaniel17@gmail.com>
2023-02-27 16:39:53 +0100

    prototypes - prototypal inheritance - prototype chain - built in objects

--- 09-OOP/script.js
+++ 09-OOP/script.js
@@ -35,6 +35,8 @@ console.log("%c\n--- prototypes ---", "color: #28b487")
 /*
 BETTER WAY TO CREATE A METHOD
 now there exists only one copy and every object created with this constructor function can use it on them selves
+
+PROTOTYPAL INHERITANCE / DELEGATION
 */
 Person.prototype.calcAge = function () {
   console.log(this.firstName + "'s age", 2037 - this.birthYear)
@@ -42,6 +44,33 @@ Person.prototype.calcAge = function () {
 
 jack.calcAge()
 
+/* 
+Person.prototype = jack.__proto__
+Person.prototype = jonas.__proto__
+
+Person.prototype is not the prototype of Person, but the prototype of objects created by Person
+*/
+
 // instanceof
 console.log("%c\n--- instanceof ---", "color: #28b487")
 console.log("jack is a instanceof on Person", jack instanceof Person)
+
+/* 
+Prototype Chain
+looking in prototypes untill we find the property / method
+
+Object.prototype is the top of the prototype chain
+*/
+
+Array.prototype.unique = function () {
+  return [...new Set(this)]
+}
+/* 
+NOT A GOOD IDEA
+1. JS could add the same method in the future and it may work differently
+2. When working in a team, multiple dvelopers may add the same method with different logic, this would create bugs
+*/
+
+console.log([1, 1, 1, 12, 2, 2, 2, 2, 4, 6, 7, 8, 8, 8, 8, 8].unique())
+
+// Thst's why we can call methods on functions, they are objects and objects have prototypes
