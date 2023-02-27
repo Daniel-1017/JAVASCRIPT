41260c94c2383b6fa7f5fa009fb5e11ae81ea6ae
Daniel-17 <frimudaniel17@gmail.com>
2023-02-27 17:04:08 +0100

    challenge

--- 09-OOP/script.js
+++ 09-OOP/script.js
@@ -74,3 +74,37 @@ NOT A GOOD IDEA
 console.log([1, 1, 1, 12, 2, 2, 2, 2, 4, 6, 7, 8, 8, 8, 8, 8].unique())
 
 // Thst's why we can call methods on functions, they are objects and objects have prototypes
+
+console.log("%c\n--- challenge 1 ---", "color: #28b487")
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make
+  this.speed = speed
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10
+  console.log(`${this.make} is goind at ${this.speed} km/h`)
+}
+
+Car.prototype.brake = function () {
+  this.speed -= 5
+  console.log(`${this.make} is goind at ${this.speed} km/h`)
+}
+
+const bmw = new Car("BMW", 120)
+const mercedes = new Car("MERCEDES", 95)
+bmw.accelerate()
+bmw.accelerate()
+bmw.brake()
+bmw.accelerate()
+bmw.brake()
