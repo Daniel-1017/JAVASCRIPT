0bc845c098567ec1b73d603ef19adcbc5fb03f76
Daniel-17 <frimudaniel17@gmail.com>
2023-02-28 16:44:39 +0100

    accound class example

--- 09-OOP/script.js
+++ 09-OOP/script.js
@@ -383,3 +383,47 @@ tesla.accelerate()
 tesla.accelerate()
 tesla.brake()
 console.log(tesla)
+
+// Class example
+console.log("%c\n--- class example ---", "color: #28b487")
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner
+    this.currency = currency
+    this.pin = pin
+    this.movements = []
+    this.locale = navigator.language
+
+    console.log(`Thanks for opening an account, ${owner}`)
+  }
+
+  // Public interface of our objects
+  deposit(val) {
+    this.movements.push(val)
+  }
+
+  withdraw(val) {
+    this.deposit(-val)
+  }
+
+  approveLoan(val) {
+    return true
+  }
+
+  reqLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val)
+      console.log("Loan approved")
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111)
+console.log(acc1.pin)
+
+/* 
+BAD IDEA, USE THE PUBLIC INTERFACE INSTEAD
+acc1.movements.push(100)
+acc1.movements.push(-230)
+*/
