ebbb3e97b8bbc208c79188297999bd929f68bac6
Daniel-17 <frimudaniel17@gmail.com>
2023-03-06 12:37:10 +0100

    listening for the load & hashchange event

--- 13-forkify/src/js/controller.js
+++ 13-forkify/src/js/controller.js
@@ -30,11 +30,14 @@ const renderSpinner = parentEl => {
 
 const showRecipe = async () => {
   try {
-    // 1) Loading recipe
+    const id = window.location.hash.slice(1)
+
+    if (!id) return
 
     renderSpinner(recipeContainer)
+    // 1) Loading recipe
     // prettier-ignore
-    const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886")
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
     const data = await res.json()
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`)
@@ -153,3 +156,4 @@ const showRecipe = async () => {
   }
 }
 showRecipe()
+;["hashchange", "load"].forEach(ev => window.addEventListener(ev, showRecipe))
