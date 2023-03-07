a22fc7f828ff446b8e0296c1329fe7600344fe74
Daniel-17 <frimudaniel17@gmail.com>
2023-03-07 10:58:00 +0100

    implementing search results - part 1

--- 13-forkify/src/js/controller.js
+++ 13-forkify/src/js/controller.js
@@ -1,12 +1,9 @@
-import * as model from "./model.js";
-import recipeView from "./views/recipeView.js";
+import * as model from "./model";
+import recipeView from "./views/recipeView";
+import searchView from "./views/searchView";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipe = async () => {
   try {
@@ -24,9 +21,25 @@ const controlRecipe = async () => {
     recipeView.renderError();
   }
 };
-controlRecipe();
+
+const controlSearchResults = async () => {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const init = () => {
   recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
--- 13-forkify/src/js/model.js
+++ 13-forkify/src/js/model.js
@@ -3,12 +3,16 @@ import { getJSON } from "./helpers";
 
 export const state = {
   recipe: {},
+  search: {
+    query: "",
+    results: [],
+  },
 };
 
 export const loadRecipe = async id => {
   try {
     // prettier-ignore
-    const data = await getJSON(`${API_URL}/${id}`)
+    const data = await getJSON(`${API_URL}${id}`)
 
     const { recipe } = data.data;
     state.recipe = {
@@ -25,3 +29,23 @@ export const loadRecipe = async id => {
     throw err;
   }
 };
+
+export const loadSearchResults = async query => {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+loadSearchResults("pizza");
